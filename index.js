faceb15d8dd103b145e5f71d1c96e1dbd76179e9
Egggggg <mrhumanman2@gmail.com>
2022-08-28 00:59:44 -0400

    y;eah

---
+++ index.js
@@ -0,0 +1,34 @@
+const { Client, GatewayIntentBits } = require("discord.js");
+const { token, users } = require("./config.json");
+
+const client = new Client({
+	intents: [
+		GatewayIntentBits.Guilds,
+		GatewayIntentBits.GuildMessages,
+		GatewayIntentBits.MessageContent,
+	]
+});
+
+client.once("ready", () => {
+	console.log("Ready");
+});
+
+client.on("messageCreate", async (msg) => {
+	if (msg.content === "clean up this damn channel") {
+		if (msg.author.id in users) {
+			if (users[msg.author.id].includes(msg.channelId)) {
+				const reply = msg.reference;
+				
+				if (reply.channelId === msg.channelId) {
+					if ("messageId" in reply) {
+						const messages = await msg.channel.messages.fetch({ after: reply.messageId });
+
+						await msg.channel.bulkDelete(messages, true);
+					}
+				}
+			}
+		}
+	}
+});
+
+client.login(token);
